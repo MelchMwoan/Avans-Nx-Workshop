c1fc45ac6ebdf0ae2837b54fe53b79663ec5d2ab
MelchMwoan <Paarlbergamy@gmail.com>
2023-11-09 11:03:19 +0100

    Added default navbar

--- apps/share-a-meal-web/src/app/app.component.ts
+++ apps/share-a-meal-web/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { FeaturesModule } from '@avans-nx-workshop/share-a-meal/features';
+import { UiModule } from '@avans-nx-workshop/share-a-meal/ui';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule, UiModule],
   selector: 'avans-nx-workshop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'share-a-meal-web';
+  ngOnInit(): void {
+      initFlowbite();
+  }
 }
