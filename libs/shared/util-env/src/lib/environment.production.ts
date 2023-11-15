c4f9e9ece939f9b49aab79501584ec81ea88adc3
MelchMwoan <Paarlbergamy@gmail.com>
2023-11-15 22:49:57 +0100

    Updated production api url to use azure api

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -2,5 +2,5 @@ import { IEnvironment } from "./environment.interface";
 
 export const environment : IEnvironment = {
     production: true,
-    dataApiUrl: "productionUrl"
+    dataApiUrl: "https://ttvd-trainingen-api-nestjs.azurewebsites.net/api"
 }
