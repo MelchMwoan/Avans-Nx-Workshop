72b754c0a6b81dc9a178a027e65435155e90b30c
MelchMwoan <Paarlbergamy@gmail.com>
2023-11-17 09:39:01 +0100

    Updated About

--- libs/ttvd-trainingen/features/src/lib/about/about.component.ts
+++ libs/ttvd-trainingen/features/src/lib/about/about.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'avans-nx-workshop-about',
   templateUrl: './about.component.html',
   styles: [],
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  imagePath?: string;
+  ngOnInit(): void {
+    this.imagePath = '/assets/IndivDomain.drawio.png';
+  }
+}
