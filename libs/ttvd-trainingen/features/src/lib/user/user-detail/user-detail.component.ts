6531641c62840fee7248b46848f33790273f8f9e
MelchMwoan <Paarlbergamy@gmail.com>
2023-11-20 22:43:15 +0100

    Updated detail page

--- libs/ttvd-trainingen/features/src/lib/user/user-detail/user-detail.component.ts
+++ libs/ttvd-trainingen/features/src/lib/user/user-detail/user-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { IUser } from '@avans-nx-workshop/shared/api';
+import { IPlayer, ITrainer, IUser } from '@avans-nx-workshop/shared/api';
 import { Subscription } from 'rxjs';
 import { UserService } from '../user.service';
 import { ActivatedRoute } from '@angular/router';
@@ -11,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
   styles: [],
 })
 export class UserDetailComponent implements OnInit, OnDestroy {
-  user: IUser | null = null;
+  user: (IPlayer | ITrainer) | null = null;
   subscription: Subscription | undefined = undefined;
   routeSub: Subscription | undefined = undefined;
 
@@ -30,4 +30,8 @@ export class UserDetailComponent implements OnInit, OnDestroy {
       if(this.routeSub) this.routeSub.unsubscribe();
   }
 
+  isTrainer(user: IPlayer | ITrainer): user is ITrainer {
+    if(user == null) return false;
+    return 'loan' in user;
+  }
 }
