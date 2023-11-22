216fa7b68bd6aead607cfc12b1566af973d08e8f
MelchMwoan <Paarlbergamy@gmail.com>
2023-11-22 13:30:14 +0100

    Added login/logout to navbar

--- libs/ttvd-trainingen/ui/src/lib/navbar/navbar.component.ts
+++ libs/ttvd-trainingen/ui/src/lib/navbar/navbar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'libs/ttvd-trainingen/features/src/lib/auth/auth.service';
 
 @Component({
   selector: 'avans-nx-workshop-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+
+export class NavbarComponent {
+  constructor(public authService: AuthService) {}
+  logout() {
+    this.authService.logout();
+  }
+}
