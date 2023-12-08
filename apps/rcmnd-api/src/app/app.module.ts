10a5a42c28e1c7f5dc6c4c9d7f655eee92d8c90a
MelchMwoan <Paarlbergamy@gmail.com>
2023-12-08 13:54:07 +0100

    setup for rcmnd api

--- apps/rcmnd-api/src/app/app.module.ts
+++ apps/rcmnd-api/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-
+import { Neo4jBackendModule } from '@avans-nx-workshop/backend/neo4j';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Neo4jModule } from 'nest-neo4j/dist'
 
 @Module({
-  imports: [],
+  imports: [
+    Neo4jModule.forRoot({
+      scheme: 'bolt+s',
+      host: '92d8bfad.databases.neo4j.io',
+      port: 7687,
+      username: process.env.NEO4J_USER,
+      password: process.env.NEO4J_PASSWORD
+    }),
+    Neo4jBackendModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
