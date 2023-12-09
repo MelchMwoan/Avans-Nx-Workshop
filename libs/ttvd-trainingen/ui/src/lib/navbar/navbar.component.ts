832e0232b1806aefa414660dc1021c73b426460a
MelchMwoan <Paarlbergamy@gmail.com>
2023-12-09 14:24:33 +0100

    fixed navbar profile button

--- libs/ttvd-trainingen/ui/src/lib/navbar/navbar.component.ts
+++ libs/ttvd-trainingen/ui/src/lib/navbar/navbar.component.ts
@@ -8,16 +8,19 @@ import { AuthService } from 'libs/ttvd-trainingen/features/src/lib/auth/auth.ser
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  name?: string | null;
   email?: string | null;
   constructor(public authService: AuthService) {
     authService.currentUser$.subscribe((result) => {
       if (result == undefined) {
+        this.name = null;
         this.email = null;
       } else {
-        this.email =
+        this.name =
           (result as any)?.results.user.firstName +
           ' ' +
           (result as any)?.results.user.lastName;
+        this.email = (result as any)?.results.user.email;
       }
     });
   }
